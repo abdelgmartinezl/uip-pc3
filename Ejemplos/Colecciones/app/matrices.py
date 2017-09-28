d854f115b8d23f6587902f8d33a07a3127d50525
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-09-27 21:14:30 -0500

    Manejo de matrices

--- Ejemplos/Colecciones/app/matrices.py
+++ Ejemplos/Colecciones/app/matrices.py
@@ -1,3 +1,10 @@
 if __name__ == "__main__":
     mi_tabla = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
     print(mi_tabla)
+
+    print(mi_tabla[1][1])
+
+    for fila in mi_tabla:
+        for columna in fila:
+            print(columna*5)
+
