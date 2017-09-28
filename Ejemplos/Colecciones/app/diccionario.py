f6913cb3913f0b5985840dc96737bcd9e5c0482f
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-09-27 20:28:36 -0500

    Manejo de diccionarios

--- Ejemplos/Colecciones/app/diccionario.py
+++ Ejemplos/Colecciones/app/diccionario.py
@@ -2,3 +2,8 @@ if __name__ == '__main__':
     canicas = {"rojo": 17, "verde": 6, "amarillo": 1}
 
     print("Yo tengo " + str(canicas['verde']) + " verdes.")
+
+    try:
+        print("Yo tengo" + str(canicas['azul']) + " azules.")
+    except:
+        print("No hay canicas azules.")
