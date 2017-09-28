5c8859a66a3512f50516802cf853973b64631757
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-09-27 21:38:25 -0500

    Manejo de cadenas

---
+++ Ejemplos/Colecciones/app/cadenas.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    palabra_magica = "alakazam"
+
+    print(len(palabra_magica))
+    print(palabra_magica.index("k"))
+    print(palabra_magica[6])
+    print(palabra_magica[3:5])
