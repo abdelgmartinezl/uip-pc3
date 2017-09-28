921eac69a2f665f0b6e29e743520ebd3494e21f4
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-09-27 21:29:00 -0500

    Manejo de rangos

--- Ejemplos/Colecciones/app/rangos.py
+++ Ejemplos/Colecciones/app/rangos.py
@@ -2,3 +2,7 @@ if __name__ == "__main__":
     print(list(range(10)))
     print(list(range(1, 11)))
     print(list(range(1, 11, 2)))
+
+    for x in range(2, 11, 2):
+        if x == 8:
+            print("Se lo ganó")
