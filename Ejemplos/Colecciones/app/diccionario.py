cf1fa4fe27d4ba4f1e0495eadf1e845742af32ee
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-09-27 20:50:16 -0500

    Manejo de diccionarios

--- Ejemplos/Colecciones/app/diccionario.py
+++ Ejemplos/Colecciones/app/diccionario.py
@@ -14,7 +14,10 @@ if __name__ == '__main__':
 
     nuevo_color = input("Que color de canica compraste: ")
     nuevas = int(input("Cuantas canicas " + nuevo_color + " son: "))
-    canicas[nuevo_color] = nuevas
+    if nuevo_color in canicas.keys():
+        canicas[nuevo_color] += nuevas
+    else:
+        canicas[nuevo_color] = nuevas
     print(canicas)
 
     for color in canicas.keys():
