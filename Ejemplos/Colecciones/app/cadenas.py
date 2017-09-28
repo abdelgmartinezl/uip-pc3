a0a6b59151b6e07fb0501bd4f80483f7f2c109b6
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-09-27 21:40:15 -0500

    Manejo de cadenas

--- Ejemplos/Colecciones/app/cadenas.py
+++ Ejemplos/Colecciones/app/cadenas.py
@@ -1,7 +1,11 @@
 if __name__ == "__main__":
-    palabra_magica = "alakazam"
+    palabra_magica = "abracadabra, alakazam"
 
     print(len(palabra_magica))
     print(palabra_magica.index("k"))
     print(palabra_magica[6])
     print(palabra_magica[3:5])
+
+    letra = input("Ingrese una letra: ")
+    if letra in palabra_magica:
+        print(palabra_magica.index(letra))
