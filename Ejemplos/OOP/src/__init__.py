7a29d09dd094e3e6e3bf5d1bb9f4fca76d1a4b73
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-18 19:23:49 -0500

    Programación orientada a objetos.

--- Ejemplos/OOP/src/__init__.py
+++ Ejemplos/OOP/src/__init__.py
@@ -17,3 +17,14 @@ if __name__ == '__main__':
     petra = Cliente('Petra Petrov', 1000.0)
     print("Quien es el mejor cliente?")
     print(petra.nombre + " con $" + str(petra.saldo))
+
+    deposito = float(input("Cantidad a depositar: "))
+    saldo = petra.depositar(deposito)
+
+    print("El saldo provisional de " + petra.nombre + " es " + str(petra.saldo))
+
+    retiro = float(input("Cantidad a retirar: "))
+    try:
+        petra.retirar(retiro)
+    except RuntimeError as e:
+        print(str(e))
