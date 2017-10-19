ccb98d4ab9bc58b43387e1ef169d096f1ad74c7e
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-18 19:28:57 -0500

    Atributos estáticos

---
+++ Ejemplos/OOP/src/carro.py
@@ -0,0 +1,14 @@
+class Carro(object):
+    llantas = 4
+
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def pitar():
+        print("Zgschhfd Zgschhfd")
+
+if __name__ == '__main__':
+    camaro = Carro('Chevrolet', 'Camaro')
+    print(camaro.llantas)
+    print(Carro.llantas)
