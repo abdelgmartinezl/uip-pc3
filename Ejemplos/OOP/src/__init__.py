167d8c1650e5a2540cc946e6d5d3354394401af5
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-18 19:59:26 -0500

    Atributos privados

--- Ejemplos/OOP/src/__init__.py
+++ Ejemplos/OOP/src/__init__.py
@@ -1,7 +1,10 @@
 class Cliente(object):
+    __total_clientes = 0
+
     def __init__(self, nombre, saldo=0.0):
         self.nombre = nombre
         self.saldo = saldo
+        Cliente.__total_clientes += 1
 
     def retirar(self, cantidad):
         if cantidad > self.saldo:
@@ -28,3 +31,9 @@ if __name__ == '__main__':
         petra.retirar(retiro)
     except RuntimeError as e:
         print(str(e))
+
+    #print("El total de clientes es " + str(Cliente.__total_clientes))
+    juan = Cliente('Juan Juan')
+    #print("El total de clientes es " + str(Cliente.__total_clientes))
+    jose = Cliente('Jose Jose', 150.0)
+    #print("El total de clientes es " + str(Cliente.__total_clientes))
