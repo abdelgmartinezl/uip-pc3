4ad417c0a40eba7c9c7a3ceb7a42b3e602d81c09
Abdel Gadiel Martinez Lassonde <abdel.g.martinez.l@gmail.com>
2017-11-01 20:01:23 -0500

    Manejo de expresiones regulares

--- Ejemplos/ExpresionesRegulares/app/__init__.py
+++ Ejemplos/ExpresionesRegulares/app/__init__.py
@@ -10,8 +10,8 @@ for palabra in palabras:
     else:
         print("La palabra " + palabra + " no cumple.")
 
-patron = '.[a-z]sa'
-palabras = ['casa', 'cesa', 'cosa', 'tasa']
+patron = '\\\[a-z]sa'
+palabras = ['casa', 'cesa', 'cosa', 'tasa', '\\asa']
 
 for palabra in palabras:
     if re.match(patron, palabra):
@@ -20,7 +20,25 @@ for palabra in palabras:
         print("La palabra " + palabra + " no cumple.")
 
 patron = '[A-Za-z][6-9a-z].\.exe'
-palabras = ['1', '15', '7']
+palabras = ['A7a.exe', 'tns.exe', 'X1..exe']
+
+for palabra in palabras:
+    if re.match(patron, palabra):
+        print("La palabra " + palabra + " cumple.")
+    else:
+        print("La palabra " + palabra + " no cumple.")
+
+patron = '\d\w\w\w\d\.exe'
+palabras = ['1aaa1.exe', '10aa1.exe', '11111.exe', '1,,,1.exe']
+
+for palabra in palabras:
+    if re.match(patron, palabra):
+        print("La palabra " + palabra + " cumple.")
+    else:
+        print("La palabra " + palabra + " no cumple.")
+
+patron = '[A-Z][a-z]+\s[A-Z](\.|[a-z]+)'
+palabras = ['Abdel M.', 'Abdel M', 'Abdel Martinez', 'Abdulah Ramadahzunzo.']
 
 for palabra in palabras:
     if re.match(patron, palabra):
