5bbc5aed0a05bec1aa14531b9d10a073c8da2197
Abdel Gadiel Martinez Lassonde <abdel.g.martinez.l@gmail.com>
2017-11-01 19:38:09 -0500

    Manejo de expresiones regulares

--- Ejemplos/ExpresionesRegulares/app/__init__.py
+++ Ejemplos/ExpresionesRegulares/app/__init__.py
@@ -1,8 +1,26 @@
 
 import re
 
-patron = 'ra(..|...)ta'
-palabras = ['ratata', 'rapero', 'raqueta', 'rata']
+patron = '[0-7]'
+palabras = ['1', '15', '7']
+
+for palabra in palabras:
+    if re.match(patron, palabra):
+        print("La palabra " + palabra + " cumple.")
+    else:
+        print("La palabra " + palabra + " no cumple.")
+
+patron = '.[a-z]sa'
+palabras = ['casa', 'cesa', 'cosa', 'tasa']
+
+for palabra in palabras:
+    if re.match(patron, palabra):
+        print("La palabra " + palabra + " cumple.")
+    else:
+        print("La palabra " + palabra + " no cumple.")
+
+patron = '[A-Za-z][6-9a-z].\.exe'
+palabras = ['1', '15', '7']
 
 for palabra in palabras:
     if re.match(patron, palabra):
