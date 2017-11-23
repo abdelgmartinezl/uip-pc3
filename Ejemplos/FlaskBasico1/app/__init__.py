1265ad29957d25cb7395f7782b46396092b94df0
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-11-22 19:54:51 -0500

    Flask básico

--- Ejemplos/FlaskBasico1/app/__init__.py
+++ Ejemplos/FlaskBasico1/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -20,7 +20,21 @@ def puede_votar(edad):
         return 'Puede votar'
     else:
         return 'Menor de edad'
-    
+
+@app.route('/admin')
+def hola_admin():
+    return'Hola Admin!'
+
+@app.route('/invitado/<invitado>')
+def hola_invitado(invitado):
+    return 'Hola %s! Eres invitado' % invitado
+
+@app.route('/usuario/<nombre>')
+def hola_usuario(nombre):
+    if nombre == 'admin':
+        return redirect(url_for('hola_admin'))
+    else:
+        return redirect(url_for('hola_invitado',invitado=nombre))
 
 if __name__ == '__main__':
     app.run()
