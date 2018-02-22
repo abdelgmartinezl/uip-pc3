35b1e0b23eee3c58838a10027a96840d15494ca1
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2018-02-21 20:42:14 -0500

    Se te cayó un cable blanco

--- Ejemplos/ejemplo36/__init__.py
+++ Ejemplos/ejemplo36/__init__.py
@@ -11,8 +11,10 @@ if __name__ == '__main__':
         if any(d.lower() in dia.lower() for d in fin_de_semana):
             print(dia)
 
-
-    ruta2 = 'Ejemplos/ejemplo36/pereza.txt'
+    ruta2 = 'Ejemplos\\ejemplo36\\pereza.txt'
     archivo_pereza = open(ruta2, 'w')
-    archivo_pereza.write("Tengo pereza")
+    archivo_pereza.write("Tengo pereza\n")
     archivo_pereza.write("Espero que el prof termine temprano")
+
+    archivo.close()
+    archivo_pereza.close()
