daae832f36b31aede4f723975add573413286b55
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2018-05-30 20:03:02 -0500

    Ejemplo de ventas mensuales

---
+++ Ejemplos/Vendedor/app/__init__.py
@@ -0,0 +1,25 @@
+# Soy un comentario
+print('''VENDESOFT 1.0
+         *************
+         El mejor software para registrar ventas...
+         Y no tienes que pagar ningun centavo!
+      ''')
+
+while True:
+    nombre = input("\nNombre: ") # Dale pue
+    venta_mensual = float(input('Venta Mensual: '))
+
+    if venta_mensual > 100_000.00:
+        print("Eres un vendedor estrella!!!")
+    elif venta_mensual > 90000.00:
+        print("Te falta poco para la meta...")
+    elif venta_mensual > 50000:
+        print("Sobreviviste :)")
+    elif venta_mensual > 0.00:
+        print("Larga a vender mas :@")
+    else:
+        print("Que haces aqui? DESPEDIDO!")
+
+    opcion = input("Desea continuar (S/N)?")
+    if opcion == 'N' or opcion == 'n':
+        break
